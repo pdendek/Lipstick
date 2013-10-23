ed0702788505e26a14ecd5369fff02cef310ae24
pdendek <pdendek@icm.edu.pl>
2013-10-23 20:24:45 +0200

    amendments to CDH4.3.0

--- lipstick-server/grails-app/conf/BuildConfig.groovy
+++ lipstick-server/grails-app/conf/BuildConfig.groovy
@@ -26,6 +26,7 @@ grails.project.dependency.resolution = {
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
+	excludes 'log4j', 'jcl-over-slf4j', /*'slf4j-api', 'slf4j-log4j12',*/ 'tomcat','jsp-api','commons-el','servlet-api','grails-plugin-log4j'
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
@@ -36,7 +37,8 @@ grails.project.dependency.resolution = {
         grailsHome()
         grailsCentral()
         mavenCentral()
-
+	mavenRepo "https://repository.cloudera.com/artifactory/cloudera-repos/"
+	
         // uncomment these to enable remote dependency resolution from public Maven repositories
         mavenCentral()
         mavenLocal()
@@ -47,8 +49,11 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         runtime 'mysql:mysql-connector-java:5.1.16'
-        runtime 'org.apache.hadoop:hadoop-core:1.1.0'
-        compile 'org.apache.pig:pig:0.11.1'
+        runtime 'org.apache.hadoop:hadoop-core:2.0.0-mr1-cdh4.3.0'
+        runtime 'org.apache.hadoop:hadoop-common:2.0.0-cdh4.3.0'
+	runtime 'org.slf4j:slf4j-api:1.7.5'
+	runtime 'org.slf4j:slf4j-log4j12:1.7.5'
+        compile 'org.apache.pig:pig:0.11.0-cdh4.3.0'
     }
 
     plugins {
